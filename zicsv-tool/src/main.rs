ffb96b29fb37ace0e2cd12e286b24d3ed700f3a6
Ivan Mironov <mironov.ivan@gmail.com>
2017-12-02 19:00:07 +0300

    Enable more clippy lints.

--- zicsv-tool/src/main.rs
+++ zicsv-tool/src/main.rs
@@ -1,6 +1,17 @@
 #![forbid(unsafe_code)]
 #![warn(unused_results)]
 
+#![cfg_attr(feature = "cargo-clippy", warn(filter_map))]
+#![cfg_attr(feature = "cargo-clippy", warn(if_not_else))]
+#![cfg_attr(feature = "cargo-clippy", warn(mut_mut))]
+#![cfg_attr(feature = "cargo-clippy", warn(non_ascii_literal))]
+#![cfg_attr(feature = "cargo-clippy", warn(option_map_unwrap_or))]
+#![cfg_attr(feature = "cargo-clippy", warn(option_map_unwrap_or_else))]
+#![cfg_attr(feature = "cargo-clippy", warn(single_match_else))]
+#![cfg_attr(feature = "cargo-clippy", warn(wrong_pub_self_convention))]
+#![cfg_attr(feature = "cargo-clippy", warn(use_self))]
+#![cfg_attr(feature = "cargo-clippy", warn(used_underscore_binding))]
+
 extern crate failure;
 
 #[macro_use]
